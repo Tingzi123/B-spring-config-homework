aa41edb62d2b0765a218f02b560a92e0306a9e60
Tingzi123 <1044373556@qq.com>
2020-11-17 23:35:22 +0800

    init

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplication.java
@@ -1,25 +1,14 @@
+
 package com.thoughtworks.capability.gtb.demospringconfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
-public class DemoSpringConfigApplication implements ApplicationRunner {
-
-	@Autowired
-	private MailConfig mailConfig;
+public class DemoSpringConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringConfigApplication.class, args);
 	}
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		System.out.println(mailConfig);
-	}
 }
