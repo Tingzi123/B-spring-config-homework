2f5a3b67fd7fe800e6e554ebd918ca2cc5e18f6d
Tingzi123 <1044373556@qq.com>
2020-11-17 23:23:50 +0800

    feat @ConfigurationProperties read list map object

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -1,13 +1,26 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
-@ConfigurationProperties("mail")
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+
+    private List<String> defaultRecipients;
+
+    private Map<String, String> additionalHeaders;
+
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +46,39 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map<String, String> getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map<String, String> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + credentials.toString() +
                 '}';
     }
 }
