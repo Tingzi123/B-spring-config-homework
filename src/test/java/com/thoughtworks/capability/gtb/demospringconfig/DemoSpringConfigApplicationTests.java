6ca1b39924e758d84bbc78a57f1392cf5e5e22ac
Tingzi123 <1044373556@qq.com>
2020-11-18 01:06:14 +0800

    use TestPropertySourceUtils

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,11 +1,19 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import javafx.application.Application;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(
+		initializers = PropertyOverrideContextInitializer.class,
+		classes = Application.class)
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
 
--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/PropertyOverrideContextInitializer.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/PropertyOverrideContextInitializer.java
@@ -1,18 +1,9 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
-import javafx.application.Application;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration(
-        initializers = PropertyOverrideContextInitializer.class,
-        classes = Application.class)
 public class PropertyOverrideContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     static final int LEVEL_NUMBER = 0;
