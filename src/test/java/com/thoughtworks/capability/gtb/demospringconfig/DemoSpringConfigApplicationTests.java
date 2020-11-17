4f69a478a81ae8b5cddb9757a650e4628f49012d
Tingzi123 <1044373556@qq.com>
2020-11-17 23:48:14 +0800

    use the same name properties file

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private LevelController levelController;
 
+	/*@Test
+	void contextLoads() {
+	}*/
+
+	@Test
+	public void shouldReturnBasic(){
+		assertEquals("basic",levelController.getLevel());
+	}
 }
